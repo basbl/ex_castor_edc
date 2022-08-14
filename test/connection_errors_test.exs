9c1a85804b608af20cdfac76dbfff5136f40324e
basbl <bas.bloembergen@castoredc.com>
2022-08-14 16:02:29 +0200

    Use Tesla.Mock adapter in connecton error test

--- test/connection_errors_test.exs
+++ test/connection_errors_test.exs
@@ -1,54 +1,40 @@
 defmodule CastorEDCTest.ConnectionErrors do
   use ExUnit.Case, async: true
-  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
 
   import CastorEDC
 
-  @client CastorEDC.Client.new(%{access_token: "supersecretaccesstoken"})
+  @client CastorEDC.Client.new(
+            %{access_token: "supersecretaccesstoken"},
+            "https://data.castoredc.com/",
+            adapter: Tesla.Mock
+          )
 
-  # These tests verify that we will return `{:error, :reason}` tuples on
-  # non-HTTP related errors, like timeouts and refused connections.
-  #
-  # In reality, :reason is an atom (at least with Hackney, this may be
-  # adapter-specific behavior?), however due to a quirk in ExVCR these atoms
-  # are converted to strings on save:
-  #
-  # https://github.com/parroty/exvcr/blob/69aaa49c83de34814dc66253e5819583077cf145/lib/exvcr/adapter/hackney/converter.ex#L96-L101
-  #
-  # ..but not converted back from string to atom on load/restore:
-  #
-  # https://github.com/parroty/exvcr/blob/69aaa49c83de34814dc66253e5819583077cf145/lib/exvcr/adapter/hackney/converter.ex#L8-L21
-  #
-  # As such these tests pattern-match on strings while in reality, end-users
-  # should pattern-match on atoms.
+  setup do
+    # Intercept all requests to always return {:error, :econnrefused}
+    #
+    # For more information, see:
+    #
+    # - https://hexdocs.pm/tesla/Tesla.Mock.html
+    Tesla.Mock.mock_global(fn _env ->
+      {:error, :econnrefused}
+    end)
+
+    :ok
+  end
 
   test "get" do
-    use_cassette "errors/get" do
-      {:error, "econnrefused"} = get("api/", @client)
-    end
+    {:error, :econnrefused} = get("api/", @client)
   end
 
   test "post" do
-    use_cassette "errors/post" do
-      {:error, "econnrefused"} = post("api/", @client, %{})
-    end
+    {:error, :econnrefused} = post("api/", @client, %{})
   end
 
   test "patch" do
-    use_cassette "errors/patch" do
-      {:error, "econnrefused"} = patch("api/", @client, %{})
-    end
+    {:error, :econnrefused} = patch("api/", @client, %{})
   end
 
   test "authenticate" do
-    use_cassette "errors/authenticate" do
-      client =
-        CastorEDC.Client.new(%{
-          client_id: "0b362196-09ad-4d09-b5bf-27a515bbc358",
-          client_secret: "secret"
-        })
-
-      {:error, "econnrefused"} = CastorEDC.authenticate(client)
-    end
+    {:error, :econnrefused} = CastorEDC.authenticate(@client)
   end
 end
