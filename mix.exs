32b921f6409e4114363556cbd16704eb11aa74f0
basbl <basbloembergen@gmail.com>
2023-04-26 21:23:12 +0200

    Bump version to 0.4.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule CastorEDC.MixProject do
   use Mix.Project
 
-  @version "0.3.0"
+  @version "0.4.0"
   @source_url "https://github.com/basbl/ex_castor_edc/"
   @homepage_url "https://www.castoredc.com/"
 
